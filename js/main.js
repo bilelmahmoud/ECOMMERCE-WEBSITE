b203c3d4b3c5f538df8fbd7f9126ee15ce9df07e
bilelmahmoud <bilelbenmahmoud777@gmail.com>
2024-12-04 20:50:59 -0500

    Soumettre la version finale du site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('hi')
+window.addEventListener("scroll" , () => {
+if (window.scrollY > 100) {
+    headerElement.classList.add("scroll-down")
+}else {
+    headerElement.classList.remove("scroll-down")
+}
+ 
+})
